7b4905ee4b156c18f1d3b35777ada66835be7ab8
Daniel Lange <daniel.hoffmann@gmx.net>
2011-04-11 09:04:26 +0200

    - changed hopefully all function that convert strings to ints and doubles to use invariant culture instead of current culture. This solves problems with for instance dicom DS fields and non English cultures.

--- Dicom/Data/DicomUID.cs
+++ Dicom/Data/DicomUID.cs
@@ -93,7 +93,7 @@ namespace Dicom.Data {
 									string hex = interfaces[i].GetPhysicalAddress().ToString();
 									if (!String.IsNullOrEmpty(hex)) {
 										try {
-											long mac = long.Parse(hex, NumberStyles.HexNumber);
+											long mac = long.Parse(hex, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
 											return Generate(Implementation.ClassUID, mac);
 										} catch {
 										}
@@ -166,8 +166,8 @@ namespace Dicom.Data {
 					if (parts0[i] == parts1[i])
 						continue;
 
-					int i0 = int.Parse(parts0[i]);
-					int i1 = int.Parse(parts1[i]);
+					int i0 = int.Parse(parts0[i], CultureInfo.InvariantCulture);
+					int i1 = int.Parse(parts1[i], CultureInfo.InvariantCulture);
 
 					if (i0 == i1)
 						return 0;
